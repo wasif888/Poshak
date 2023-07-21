ba9240615144f6de07f976666d82caff95f4f256
wasif888 <mustafa888wasif@gmail.com>
2023-07-22 04:07:51 +0600

    after the changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,27 @@
-import logo from "./logo.svg";
-import "./App.css";
+import './App.css';
+import data from './data';
 
 function App() {
   return (
     <div>
       <header>
-        <a href=''>Poshak</a>
+        <a href="/">Poshak</a>
       </header>
-      <main>list products</main>
+      <main>
+        <h1>Featured products</h1>
+        <div className="products">
+          {data.products.map((products) => (
+            <div key={products.slugS} className="product">
+              <img src={products.image} alt={products.name} />
+              <div className="product-info">
+                <p>{products.name}</p>
+                <p>{products.price}</p>
+                <button>Add to cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
---
+++ client/src/data.js
@@ -0,0 +1,32 @@
+const data = {
+  products: [
+    {
+      name: 'nike-slim-shirt',
+      slug: 'adidas',
+      category: 'Shirt',
+      image: '/images/p3.jpg',
+      price: 120,
+      brand: 'nike',
+      description: 'high quality product',
+    },
+    {
+      name: 'nike-slim-pant',
+      slug: 'know',
+      category: 'pant',
+      image: '/images/p1.jpeg',
+      price: 420,
+      brand: 'nike',
+      description: 'high quality product',
+    },
+    {
+      name: 'adidas-pant',
+      slug: 'vaw',
+      category: 'Shirt',
+      image: '/images/p2.jpg',
+      price: 720,
+      brand: 'nike',
+      description: 'high quality product',
+    },
+  ],
+};
+export default data;
